cb4290e9e48417456ede85a07d276af9511131cd
Adamnson-0dinson <theawakeavatar@gmail.com>
2025-05-31 15:24:33 +0530

    (feat) video follow-along coding done
    --> ransack only for title and body. facing issue with user name and email
    --> appearance in navbar for small screen to be optimized

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,12 @@ class Post < ApplicationRecord
   validates :body, presence: true, length: { minimum: 10, maximum: 1000 }
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  def self.ransackable_attributes(auth_object = nil)
+    [ "body", "title" ] # , "user_email", "user_name" ]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    [ "user" ]
+  end
 end
